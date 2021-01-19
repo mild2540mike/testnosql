586d3542cc7a90a04e522f325ca5ff6dae51dad8
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-01-19 23:05:44 +0700

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOIZy60x3FSDhRdGeOP7ecQLjHL_2hSgw",
-    authDomain: "fir-crud-b0975.firebaseapp.com",
-    projectId: "fir-crud-b0975",
-    storageBucket: "fir-crud-b0975.appspot.com",
-    messagingSenderId: "398799064682",
-    appId: "1:398799064682:web:8c39cc47ddca0196da4413",
-    measurementId: "G-YNYR5NQCBC"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
