01e786a9df234fbe307939d38a8cb72d9f362fa4
Yanaranop Naulrahaeng <yanaranop2540mike@hotmail.com>
2021-08-05 10:48:42 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/Form'
 import TodoList from './components/TodoList'
+import React from 'react';
 
 function App() {
   return (
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+  apiKey: "AIzaSyB14zYDlbTyM7L6Lw27oHVTt-lkiCFpAIA",
+  authDomain: "nodatabase-ea8fd.firebaseapp.com",
+  projectId: "nodatabase-ea8fd",
+  storageBucket: "nodatabase-ea8fd.appspot.com",
+  messagingSenderId: "193167749709",
+  appId: "1:193167749709:web:aa45c1f4f7f06154a42204",
+  measurementId: "G-DBB25K4W67"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
